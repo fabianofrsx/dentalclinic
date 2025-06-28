b60676262eb868c2a0d98639893171887b8dd9a7
fabianofrsx <fabianoribeiro.uniao@gmail.com>
2025-06-28 12:06:25 -0300

    Atualiza ALLOWED_HOSTS para deploy Render

--- sysodont/settings.py
+++ sysodont/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from django.conf.locale.pt_BR import formats as pt_br_formats
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-*3xzec8n$)#w*dm)w76y!!p7d8ke^ytz9rods=kih^&*hbvy5t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dentalclinic-qfo6.onrender.com']
 
 
 # Application definition
@@ -144,6 +145,8 @@ DATE_INPUT_FORMATS = [
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
